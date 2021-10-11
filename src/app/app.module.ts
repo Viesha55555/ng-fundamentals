daba42b9aead72da5a67f0cbdf9a27c3bb987f82
Viesturs <vcelms@gmail.com>
2021-10-11 20:47:56 +0300

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,84 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { AppComponent } from './app.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe
+} from './events/index'
+
+import { EventsAppComponent } from './events-app.component';
+import { environment } from '../environments/environment';
+import { NavBarComponent } from './nav/navbar.component';
+import { SimpleModalComponent, CollapsibleWellComponent,JQ_TOKEN, TOASTR_TOKEN, Toastr, ModalTriggerDirective } from './common/index';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+declare let toastr:Toastr;
+declare let jQuery:any;
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    SimpleModalComponent,
+    DurationPipe,
+    ModalTriggerDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    EventService,
+    { provide: TOASTR_TOKEN, useValue: toastr},
+    { provide: JQ_TOKEN, useValue: jQuery},
+    EventRouteActivator,
+    EventListResolver,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    },
+    AuthService
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if(component.isDirty){
+    return window.confirm('You have not saved this event,do yoyu really want to cancel?')
+  }else{
+    return true;
+  }
+
+}
---
+++ src/app/user/profile.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import { TOASTR_TOKEN, Toastr } from '../common/toastr.service';
+
+@Component({
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent implements OnInit {
+    profileForm!:FormGroup  
+
+    constructor(private authService:AuthService, 
+        private router:Router,
+        @Inject(TOASTR_TOKEN) private toastr: Toastr){
+
+    }
+    ngOnInit(){
+        let firstName = new FormControl(this.authService.currentUser?.firstName);
+        let lastName = new FormControl(this.authService.currentUser?.lastName);
+        this.profileForm = new FormGroup({
+            firstName: firstName,
+            lastName: lastName
+        })
+    }
+
+    saveProfile(formValues: { firstName: string; lastName: string; }){
+        this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+        this.toastr.success('Profile saved!');
+    }
+    Cancel(){
+        this.router.navigate(['events']);
+    }
+}
